204984352c79bfa1fa7b6e3b3ddb837c8eca1f78
Rob G <crx8490@gmail.com>
2022-10-22 19:31:33 +0100

    add some components to the index

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,6 +5,9 @@
 
 package controllers
 
+import play.api.data.Form
+import play.api.data.Forms.{mapping, text}
+
 import javax.inject.{Inject, Singleton}
 import play.api.i18n.{I18nSupport, Lang}
 import play.api.mvc.{MessagesControllerComponents, Request}
@@ -12,11 +15,18 @@ import play.filters.csrf.CSRF
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.index
 
-
 import scala.concurrent.{ExecutionContext, Future}
 
 
-@Singleton class HomeController @Inject()(val mcc: MessagesControllerComponents, view: index) (implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
-def index() = Action(implicit request => Ok(view("Title", "Heading", "SomeText")))
+@Singleton class HomeController @Inject()(val mcc: MessagesControllerComponents, view: index)(implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
 
+  case class Data(val field: String) {}
+
+  val form: Form[Data] = Form[Data](
+    mapping("field" -> text)(Data.apply)(Data.unapply)
+  )
+
+  def index() = Action { implicit request =>
+    Ok(view("Title", "Heading", "SomeText"))
+  }
 }
