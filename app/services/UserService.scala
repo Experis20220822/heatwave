03f103856e7ef69c37692f9e4ed10c435ccb600f
Rob G <crx8490@gmail.com>
2022-10-25 23:22:21 +0100

    implement members

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -5,11 +5,18 @@
 
 package services
 
-import models.User
+import models.{Invoice, User}
 import repositories.UserRepository
 
 import javax.inject.Inject
+import scala.concurrent.Future
 
-class UserService @Inject() (userRepository: UserRepository){
-  def addUser(user: User) = userRepository.addUser(user)
+class UserService @Inject() (userRepository: UserRepository) extends AsyncService {
+
+
+  override def get(id: String): Future[Option[String]] = ???
+
+  override def add(user: User): Future[Option[String]] = userRepository.add(user)
+
+  override def add(invoice: Invoice): Future[Option[String]] = ???
 }
