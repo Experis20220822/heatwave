de3ced8669eda55f86e4af37b582fb8cdde39595
Phuuu <37841969+Phuuu@users.noreply.github.com>
2022-10-25 16:14:37 +0100

    adding initial pages

--- app/controllers/LoggedInController.scala
+++ app/controllers/LoggedInController.scala
@@ -11,12 +11,21 @@ import play.api.data.Forms.{mapping, text}
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
-import views.html.{InvoicePage, text_input}
+import views.html.{LoggedInPage, text_input}
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
 
-@Singleton class LoggedInController @Inject()(val mcc: MessagesControllerComponents, view: InvoicePage, textInputView: text_input)(implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
+@Singleton class LoggedInController @Inject()(val mcc: MessagesControllerComponents, view: LoggedInPage, textInputView: text_input)(implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
 
+  case class Data(val field: String) {}
+
+  val form: Form[Data] = Form[Data](
+    mapping("field" -> text)(Data.apply)(Data.unapply)
+  )
+
+  def index(): Action[AnyContent] = Action { implicit request =>
+    Ok(view(form, NormalMode))
+  }
 }
