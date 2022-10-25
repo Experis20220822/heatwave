2e6833cfbf164fede47f81650332c2979058ace6
Stuart Raynor <737342+MadStu@users.noreply.github.com>
2022-10-25 17:11:52 +0100

    Implement form elements in the InvoiceController

--- app/controllers/InvoiceController.scala
+++ app/controllers/InvoiceController.scala
@@ -7,14 +7,14 @@ package controllers
 
 import models.{Mode, NormalMode}
 import play.api.data.Form
-import play.api.data.Forms.{mapping, text}
+import play.api.data.Forms.{mapping, nonEmptyText, number, text}
 
 import javax.inject.{Inject, Singleton}
 import play.api.i18n.{I18nSupport, Lang}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request}
 import play.filters.csrf.CSRF
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
-import views.html.{InvoicePage, invoice, text_input}
+import views.html.{InvoicePage, text_input}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -22,10 +22,22 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton class InvoiceController @Inject()(val mcc: MessagesControllerComponents, view: InvoicePage, textInputView: text_input)(implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
 
-  case class Data(val field: String) {}
+  case class Data(
+                  customerDetails: String,
+                  userDetails: String,
+                  invoiceItem: String,
+                  invoiceItemPrice: Int,
+                  vatNumber: Int
+                 ) {}
 
   val form: Form[Data] = Form[Data](
-    mapping("field" -> text)(Data.apply)(Data.unapply)
+    mapping(
+      "customerDetails" -> nonEmptyText,
+      "userDetails" -> nonEmptyText,
+      "invoiceItem" -> nonEmptyText,
+      "invoiceItemPrice" -> number(min = 1),
+      "vatNumber" -> number
+    )(Data.apply)(Data.unapply)
   )
 
   def index(): Action[AnyContent] = Action { implicit request =>
