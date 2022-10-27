2f8bab2383d9f53d905c70035cab7a6ac0578d2d
Rob G <crx8490@gmail.com>
2022-10-27 10:46:55 +0100

    be able to find a user by their name and implement rudimentary auth

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -5,6 +5,7 @@
 
 package controllers
 
+import models.Mode
 import play.api.data.Form
 import play.api.data.Forms.{mapping, text}
 
@@ -12,22 +13,39 @@ import javax.inject.{Inject, Singleton}
 import play.api.i18n.{I18nSupport, Lang}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request}
 import play.filters.csrf.CSRF
+import services.UserService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import views.html.login.login
-import views.html.text_input
+import views.html.logged.LoggedInPage
 
 import scala.concurrent.{ExecutionContext, Future}
 
 
-@Singleton class LoginController @Inject()(val mcc: MessagesControllerComponents, view: login, textInputView: text_input)(implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
+@Singleton class LoginController @Inject()(val mcc: MessagesControllerComponents, view: login, userService: UserService, loggedInView: LoggedInPage)(implicit val executionContext: ExecutionContext) extends FrontendController(mcc) with I18nSupport {
 
-  case class Data(val field: String) {}
+  case class UserLoginData(username: String, password: String)
 
-  val form: Form[Data] = Form[Data](
-    mapping("field" -> text)(Data.apply)(Data.unapply)
+  val userLogin: Form[UserLoginData] = Form[UserLoginData](
+    mapping(
+      "username" -> text,
+      "password" -> text)
+    (UserLoginData.apply)(UserLoginData.unapply)
   )
 
   def index(): Action[AnyContent] = Action { implicit request =>
-    Ok(view("Sign In", "Heading", "SomeText"))
+    Ok(view(userLogin))
+  }
+
+  def login(): Action[AnyContent] = Action.async { implicit request =>
+    userLogin.bindFromRequest().fold(
+      formWithErrors => Future(BadRequest(view(formWithErrors))),
+      formData => {
+        val maybeUser = userService.getByName(formData.username)
+        maybeUser.map {
+          case Some(user) => if (user.password == formData.password) Ok(loggedInView(userLogin, user.username)) else NotFound("Password did not match.")
+          case None => NotFound("This username was not found, please try again.")
+        }
+      }
+    )
   }
 }
