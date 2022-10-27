91afd80f6aee68dedc23fc163aa81a820bb5e081
ghousedev <ghousedev@gmail.com>
2022-10-27 07:54:28 +0100

    initial commit

---
+++ test/models/InvoiceSpec.scala
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ */
+
+package models
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class InvoiceSpec extends AnyFreeSpec with Matchers {
+
+}
