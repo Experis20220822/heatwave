6516bfb00f58fef327d97ecf2ddd5e862c43d5ae
Rob G <crx8490@gmail.com>
2022-10-28 11:43:12 +0100

    rm unused test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,16 +23,11 @@ object AppDependencies {
     "org.scalatestplus"      %% "scalacheck-1-15"         % "3.2.10.0",
     "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0",
-    "org.pegdown"             % "pegdown"                 % "1.6.0",
-    "org.jsoup"               % "jsoup"                   % "1.14.3",
     "com.typesafe.play"      %% "play-test"               % PlayVersion.current,
     "org.mockito"            %% "mockito-scala"           % "1.16.42",
     "org.scalacheck"         %% "scalacheck"              % "1.15.4",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.71.0",
-    "uk.gov.hmrc"            %% "http-verbs-test-play-28" % "14.5.0",
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2",
     "com.softwaremill.diffx" %% "diffx-scalatest-should"  % "0.7.1",
-    "org.jsoup"               % "jsoup"                   % "1.15.3",
     "com.dimafeng"           %% "testcontainers-scala-scalatest" % testContainersScalaVersion,
     "com.dimafeng"           %% "testcontainers-scala-mongodb"   % testContainersScalaVersion
   ).map(_ % "test, it")
