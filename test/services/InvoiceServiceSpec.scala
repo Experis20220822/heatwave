8da33771dbcb6fe6933857ed436afbb854da72fb
ghousedev <ghousedev@gmail.com>
2022-10-28 08:21:34 +0100

    update tests

--- test/services/InvoiceServiceSpec.scala
+++ test/services/InvoiceServiceSpec.scala
@@ -35,24 +35,29 @@ class InvoiceServiceSpec
       val client: MongoClient = MongoClient(container.container.getConnectionString)
       val db: MongoDatabase = client.getDatabase("heatwave")
       val collection: MongoCollection[Document] = db.getCollection("invoices")
-      val invoice = Invoice("645b7bcc70660b7d096b7cfd", "customerDetails", "userDetails", "invoiceItem", 1 ,2)
+      val myInvoice = Invoice("645b7bcc70660b7d096b7cfd", "customerDetails", "userDetails", "invoiceItem", 1 ,2)
       val document = Document(
-        "customerDetails" -> invoice.customerDetails,
-        "userDetails" -> invoice.userDetails,
-        "invoiceItem" -> invoice.invoiceItem,
-        "invoiceItemPrice" -> invoice.invoiceItemPrice,
-        "vatNumber" -> invoice.vatNumber
+        "customerDetails" -> myInvoice.customerDetails,
+        "userDetails" -> myInvoice.userDetails,
+        "invoiceItem" -> myInvoice.invoiceItem,
+        "invoiceItemPrice" -> myInvoice.invoiceItemPrice,
+        "vatNumber" -> myInvoice.vatNumber
       )
       val invoiceService = new InvoiceService(new InvoiceRepository(db))
 
       val getId = collection.insertOne(document).head()
 
       val maybeInvoice = getId.map(r => r.getInsertedId.asObjectId().getValue.toString).flatMap(id => invoiceService.getInvoice(id))
-
-//      import scala.concurrent.duration._
-//
-//      val await: Option[Invoice] = Await.result(maybeInvoice, Duration.Inf)
-//      await must contain(Some(document.toString()))
+      val result = maybeInvoice.map {
+        case Some(invoice) => invoice mustEqual myInvoice
+        case _ => ()
+      }
+      result
     }
+  }
+
+  def documentToInvoice(d: Document): Invoice = {
+    Invoice(d("_id").toString, d("customerDetails").asString().getValue, d("userDetails").asString().getValue, d("invoiceItem").asString().getValue,
+      d("invoiceItemPrice").asInt32().getValue, d("vatNumber").asInt32().getValue)
   }
 }
